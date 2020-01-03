99ffd98eb6b4693e1d931daf2106a17cff064c15
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2020-01-03 17:20:30 +0000

    Add project files.

---
+++ ThreadPool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AdvancedTasks
+{
+    public static class Program7
+    {
+        public static int counter;
+         public static void Main()
+         {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool right here...");
+            });
+            Console.WriteLine("Press any key to exit!");
+            Console.ReadLine();
+         }
+    }
+}
